e219f338ab1803c478e71153092367db55d0d2b0
alassane8 <wadealassane2002@gmail.com>
2024-07-25 23:33:13 +0200

    feat: updated main.c ==> added menu.h + gotoligcol.h

--- code/main.c
+++ code/main.c
@@ -1,1 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+#include <time.h>
+#include <unistd.h>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include "gotoligcol.h"
+#include "menu.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main(){
+    
+    //Variables definitions
+    
+    HANDLE hConsole;
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    //Sudoku
+    system("cls");
+    gotoligcol(0, 30);
+    SetConsoleTextAttribute(hConsole, 11);
+    printf("/////////////////////////////////////// ");
+    SetConsoleTextAttribute(hConsole, 15);
+    printf("S U D O K U");
+    SetConsoleTextAttribute(hConsole, 11);
+    printf(" ///////////////////////////////////////\n");
+    gotoligcol(3, 30);
+    printf("////////////////////////////// ");
+    SetConsoleTextAttribute(hConsole, 15);
+    printf("Please, open the game page wide");
+    SetConsoleTextAttribute(hConsole, 11);
+    printf(" ////////////////////////////");
+    SetConsoleTextAttribute(hConsole, 15);
+    
+    //Copyrights
+    gotoligcol(44, 70);
+    printf("Copyright 2024 Sudoku Alassane Wade");
+
+    //Calling menu
+    menu();
+
+}
