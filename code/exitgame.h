025441cf2c6a4e4dad4f9df090d6f424a5ca913c
alassane8 <alassane.wade@edu.ece.fr>
2024-08-01 20:46:12 +0200

    feat: add exitgame.h

---
+++ code/exitgame.h
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+#include <time.h>
+#include <unistd.h>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void exitgame(unsigned char ** mat, unsigned char ** mat_bis, char *pseudo){
+
+        //variables definition
+        HANDLE hConsole;
+        hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        
+        sudokumain();
+        gotoligcol(4, 45);
+        SetConsoleTextAttribute(hConsole, 6);
+        printf("///////////////////////////// ");
+        SetConsoleTextAttribute(hConsole, 15);
+        printf("T H A N K  Y O U !");
+        SetConsoleTextAttribute(hConsole, 6);
+        printf(" /////////////////////////////");
+        gotoligcol(6, 73);
+        SetConsoleTextAttribute(hConsole, 15);
+        printf("We hope to see you soon");
+        gotoligcol(44, 70);
+        printf("Copyright 2024 Sudoku Alassane Wade");
+
+
+        free(mat);
+        free(mat_bis);
+
+        sleep(4);
+
+        system("cls");
+
+        exit(0);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
