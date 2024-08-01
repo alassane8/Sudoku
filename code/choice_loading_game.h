60baec505baa12311fc161beafe5b0476efc624d
alassane8 <alassane.wade@edu.ece.fr>
2024-08-01 20:45:53 +0200

    feat: add loading/saving menu

---
+++ code/choice_loading_game.h
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+#include <time.h>
+#include <unistd.h>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void choice_loading_game(unsigned char ** mat, unsigned char ** mat_bis, char *pseudo){
+
+    //Variables deifinition
+    int choice;
+    HANDLE hConsole;
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    // Clean page
+    system("cls");
+
+    // Display files matrices
+
+    sudokuascii();
+
+    gotoligcol(20, 70);
+    SetConsoleTextAttribute(hConsole, 6);
+    printf("Which game do you want to load ?");
+    SetConsoleTextAttribute(hConsole, 15);
+    gotoligcol(21, 70);
+    printf("1. Game 1");
+    gotoligcol(22, 70);
+    printf("2. Game 2");
+    gotoligcol(23, 70);
+    printf("3. Game 3");
+    gotoligcol(24, 70);
+    printf("4. Main menu");
+    gotoligcol(44, 70);
+    printf("Copyright 2024 Sudoku Alassane Wade");
+    gotoligcol(25, 70);
+    fflush(stdin);
+    scanf("%d", &choice);
+
+    while (choice != 4)
+    {
+        gotoligcol(25, 140);
+        SetConsoleTextAttribute(hConsole, 12);
+        printf("Error.");
+        SetConsoleTextAttribute(hConsole, 15);
+        gotoligcol(25, 70);
+        printf("  ");
+        gotoligcol(25, 70);
+        fflush(stdin);
+        scanf("%d", &choice);
+    }
+    if (choice == 4){
+        system("cls");
+        menu(mat, mat_bis, pseudo);
+    }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
