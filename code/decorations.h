2d4c8164326a38b6290fe63910aaeed56b8d494a
alassane8 <alassane.wade@edu.ece.fr>
2024-08-08 01:10:53 +0200

    feat: update decoration

--- code/decorations.h
+++ code/decorations.h
@@ -8,34 +8,44 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void decors(){
+void decors(unsigned char ** mat){
 
     //Variables Definitions
     int i;
     int j;
-    int k;
     int point = 249;
     HANDLE hConsole;
     hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
     //Grid setup
-    for (i = 0; i < 3; i++){
-        SetConsoleTextAttribute(hConsole, 6);
-        printf("+----------------------------+----------------------------+----------------------------+\n");
-        for (j = 0; j < 3; j++){
-            for (k = 0; k < 3; k++){
-                SetConsoleTextAttribute(hConsole, 6);
-                printf("| ");
-                SetConsoleTextAttribute(hConsole, 8);
-                printf("      .      .      .      ");
-            }
-            SetConsoleTextAttribute(hConsole, 6);
-            printf("|");
-            printf("\n\n");
-        }
-    }
     SetConsoleTextAttribute(hConsole, 6);
-    printf("+----------------------------+----------------------------+----------------------------+\n");
+    printf("+-----------------------------+-----------------------------+-----------------------------+\n");
+    SetConsoleTextAttribute(hConsole, 15);
+        for (i = 0; i < 9; i++){
+            if (i == 3 || i == 6){
+                SetConsoleTextAttribute(hConsole, 6);
+                printf("+-----------------------------+-----------------------------+-----------------------------+\n");
+                SetConsoleTextAttribute(hConsole, 15);
+            }
+            for (j = 0; j < 9; j++){
+                if (j == 0 || j == 3 || j == 6 || j == 9){
+                    SetConsoleTextAttribute(hConsole, 6);
+                    printf("|    ");
+                    SetConsoleTextAttribute(hConsole, 8);
+                }
+                else{
+                    SetConsoleTextAttribute(hConsole, 8);
+                    printf("|    ");
+                }
+                printf("%d", mat[i][j]);
+                printf("    ");
+                SetConsoleTextAttribute(hConsole, 6);
+            }
+        printf("|");
+        printf("\n\n");
+        }
+    SetConsoleTextAttribute(hConsole, 6);
+    printf("+-----------------------------+-----------------------------+-----------------------------+\n");
     SetConsoleTextAttribute(hConsole, 15);
 }
 
