1dd585ba169a36f329afee660bcced7edd4fa102
alassane8 <alassane.wade@edu.ece.fr>
2024-08-08 13:26:50 +0200

    feat: easy.h

--- code/easy.h
+++ code/easy.h
@@ -5,21 +5,6 @@
 #include <time.h>
 #include <unistd.h>
 
-int Unique(unsigned char ** mat, int row, int col, int num){
-    for (int j = 0; j < 9; j++) {
-        if (mat[row][j] == num) {
-            return 0;  // Number is not unique in the row
-        }
-    }
-    for (int i = 0; i < 9; i++) {
-        if (mat[i][col] == num) {
-            return 0;  // Number is not unique in the column
-        }
-    }
-    return 1;
-
-}
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void easy(unsigned char ** mat, unsigned char ** mat_bis, char *pseudo){
     
@@ -34,9 +19,7 @@ void easy(unsigned char ** mat, unsigned char ** mat_bis, char *pseudo){
     srand(time(0));
     for (int i = 0; i < 9; i++){
         for(int j = 0; j < 9; j++){
-            do{
-                randomNumber = rand() % (9) + 1;
-            }while(!Unique(mat, i, j, randomNumber));
+            randomNumber = rand() % (9) + 1;
             mat[i][j] = randomNumber;
         }
     }
