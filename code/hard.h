6a28597c29d275b58e7896d327ec40c2b8f9f276
alassane8 <alassane.wade@edu.ece.fr>
2024-08-11 12:02:04 +0200

    feat: unique values for rows

--- code/hard.h
+++ code/hard.h
@@ -9,20 +9,30 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void hard(unsigned char ** mat, unsigned char ** mat_bis, char *pseudo){
+    
     //Variables definition
-    int i;
-    int j;
+    int index;
     int randomNumber;
-    int underscore = 95;
     HANDLE hConsole;
     hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    int values[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int size = sizeof(values) / sizeof(values[0]);
 
     //Choose randomly number and positions in grid
     srand(time(0));
-    for (i = 0; i < 9; i++){
-        for(j = 0; j < 9; j++){
-            randomNumber = rand() % (9) + 1;
+    for (int i = 0; i < 9; i++){
+        for(int j = 0; j < 9; j++){
+            do {
+                // Sélectionner un indice aléatoire dans le tableau
+                index = rand() % size;
+                // Récupérer la valeur correspondante
+                randomNumber = values[index];
+            }while(randomNumber == 0);
+            values[index] = 0;
             mat[i][j] = randomNumber;
+        }
+        for (int k = 0; k < 9; k ++ ){
+            values[k] = k + 1;
         }
     }
 }
